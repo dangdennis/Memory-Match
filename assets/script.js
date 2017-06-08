3a7a45a54ba8ecef47853b93376bb2a72faa6920
Dennis Dang <dangggdennis.com>
2017-06-07 18:38:17 -0700

    version0.5 I GOT IT RUNgit add .! Needs proper resets and game conclusion

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,48 @@
-var first_src = null;
-var second_src = null;
+// **** Initialize global variables **** //
+var first_card_clicked = null;
+var second_card_clicked = null;
+var clickable = true;
 var total_possible_matches = 9;
 var match_counter = 0;
-// Currently have 4 existing cards onload
 var numberOfCards = 18;
-var img_src = ["assets/pictures/backs/back1.jpg","assets/pictures/backs/back2.jpg","assets/pictures/backs/back3.jpg","assets/pictures/backs/back4.jpg",
-"assets/pictures/backs/back5.jpg","assets/pictures/backs/back6.jpg","assets/pictures/backs/back7.jpg","assets/pictures/backs/back8.jpg",
-	"assets/pictures/backs/back9.jpg","assets/pictures/backs/back1.jpg","assets/pictures/backs/back2.jpg","assets/pictures/backs/back3.jpg",
-	"assets/pictures/backs/back4.jpg","assets/pictures/backs/back5.jpg","assets/pictures/backs/back6.jpg","assets/pictures/backs/back7.jpg","assets/pictures/backs/back8.jpg",
-	"assets/pictures/backs/back9.jpg"];
+var attempts = 0;
+var games_played = 0;
+// **** Hard-coded array of src values to be used later **** //
+var img_src = [
+	{id: "1", url: "assets/pictures/backs/back1.jpg"},
+	{id: "2", url: "assets/pictures/backs/back2.jpg"},
+	{id: "3", url: "assets/pictures/backs/back3.jpg"},
+	{id: "4", url: "assets/pictures/backs/back4.jpg"},
+	{id: "5", url: "assets/pictures/backs/back5.jpg"},
+	{id: "6", url: "assets/pictures/backs/back6.jpg"},
+	{id: "7", url: "assets/pictures/backs/back7.jpg"},
+	{id: "8", url: "assets/pictures/backs/back8.jpg"},
+	{id: "9", url: "assets/pictures/backs/back9.jpg"},
+	{id: "1", url: "assets/pictures/backs/back1.jpg"},
+	{id: "2", url: "assets/pictures/backs/back2.jpg"},
+	{id: "3", url: "assets/pictures/backs/back3.jpg"},
+	{id: "4", url: "assets/pictures/backs/back4.jpg"},
+	{id: "5", url: "assets/pictures/backs/back5.jpg"},
+	{id: "6", url: "assets/pictures/backs/back6.jpg"},
+	{id: "7", url: "assets/pictures/backs/back7.jpg"},
+	{id: "8", url: "assets/pictures/backs/back8.jpg"},
+	{id: "9", url: "assets/pictures/backs/back9.jpg"}
+];
+// **** The array of src value are shuffled here **** //
 var randomized_img_src = shuffleArray(img_src);
 
 $(document).ready(function(){
 	generateCards();
-	// addBackFaces(shuffleArray(img_src));
-	$(".flip").on("click",function(){
-		// card_clicked(this);
-		console.log('you flipped');
-		$(function(){
-			$(".flip").flip({
-				trigger: 'click',
-				axis: 'y'
-			});
-		});
+	$(".card").on("click",function(){
+		flip($(this));
+		cardMatcher($(this));
+	});
+	$("#reset").on("click",function(){
+		resetGame();
 	})
 });
 
+// ***** Function to shuffle the array of image src files ***** //
 function shuffleArray(array) {
     for (var i = array.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
@@ -36,37 +53,116 @@ function shuffleArray(array) {
     return array;
 }
 
-// Primary function for matching
-function card_clicked(card){
-	if(first_src === null){
-		first_src = $(card).find("div.back img").attr("src");
-	}
+// ***** Primary function for matching ***** //
+function cardMatcher(card) {
+    if (clickable) {
+        if (first_card_clicked === null) {
+            first_card_clicked = card;
+        } else {
+            second_card_clicked = card;
+            if( first_card_clicked.children(".back").children("img").attr("id") !== second_card_clicked.children(".back").children("img").attr("id") ) {
+            	stopClick();
+                resetFlips();
+                attempts++;
+                displayStats();
+            } else if ( first_card_clicked.children(".back").children("img").attr("id") === second_card_clicked.children(".back").children("img").attr("id") ) {
+                stopClick();
+                first_card_clicked = null;
+                second_card_clicked = null;
+                attempts++;
+                match_counter++;
+                if (match_counter === total_possible_matches) {
+                    displayStats();
+                }
+                displayStats();
+            }
+        }
+    }
 }
-
-// Generates cards onload
+// ***** Regulates user's clicks ****** //
+function stopClick(){
+    clickable = false;
+    setTimeout(resetClickable,1300);
+}
+function resetClickable(){
+    clickable = true;
+}
+// *****  Generates cards onload ***** //
 function generateCards(){
-	for(var i=0;i<numberOfCards;i++) {
-		var newCard = $("<div>").addClass("col-xs-2 card flip");
-		var newBack = $("<div>").addClass("back");
-		var latestImg = randomized_img_src.pop();
-		var newBackImg = $("<img>").attr("src",latestImg);
-		var newFront = $("<div>").addClass("front");
-		var newFrontImg = $("<img>").attr("src", "assets/pictures/legend_cardback.png");
-		newCard.append(newFront);
-		newCard.append(newBack);
-		newFront.append(newFrontImg);
-		newBack.append(newBackImg);
-		$(".game-area").append(newCard);
+	// One loop per row of cards // 
+	for(var i=0;i<numberOfCards/3;i++) {
+		// ***** div.cardContainer -> div.card -> div.front + div.back -> img ***** //
+		var obj = randomized_img_src.pop();
+		var cardContainer = $("<div>").addClass("cardContainer").appendTo(".row1");
+		var newCard = $("<div>").addClass("card").appendTo(cardContainer);
+		var newBack = $("<div>").addClass("back").appendTo(newCard);
+		var newBackImg = $("<img>").attr("id",obj.id).css('background-image', 'url(' + obj.url + ')').appendTo(newBack);
+		var newFront = $("<div>").addClass("front").appendTo(newCard);
+		var newFrontImg = $("<img>").attr("src", "assets/pictures/legend_cardback.png").appendTo(newFront);
+	}
+	// One loop per row of cards // 
+	for(var i=0;i<numberOfCards/3;i++) {
+		// ***** div.cardContainer -> div.card -> div.front + div.back -> img ***** //
+		var obj = randomized_img_src.pop();
+		var cardContainer = $("<div>").addClass("cardContainer").appendTo(".row2");
+		var newCard = $("<div>").addClass("card").appendTo(cardContainer);
+		var newBack = $("<div>").addClass("back").appendTo(newCard);
+		var newBackImg = $("<img>").attr("id",obj.id).css('background-image', 'url(' + obj.url + ')').appendTo(newBack);
+		var newFront = $("<div>").addClass("front").appendTo(newCard);
+		var newFrontImg = $("<img>").attr("src", "assets/pictures/legend_cardback.png").appendTo(newFront);
+	}
+	// One loop per row of cards // 
+	for(var i=0;i<numberOfCards/3;i++) {
+		// ***** div.cardContainer -> div.card -> div.front + div.back -> img ***** //
+		var obj = randomized_img_src.pop();
+		var cardContainer = $("<div>").addClass("cardContainer").appendTo(".row3");
+		var newCard = $("<div>").addClass("card").appendTo(cardContainer);
+		var newBack = $("<div>").addClass("back").appendTo(newCard);
+		var newBackImg = $("<img>").attr("id",obj.id).css('background-image', 'url(' + obj.url + ')').appendTo(newBack);
+		var newFront = $("<div>").addClass("front").appendTo(newCard);
+		var newFrontImg = $("<img>").attr("src", "assets/pictures/legend_cardback.png").appendTo(newFront);
+	}
+}
+// *****  Function to animate card flips  ***** //
+function flip(el) {
+	if(clickable){
+	    el.toggleClass('flipped');
 	}
 }
 
-function addBackFaces(arr){
-	$("div.back").each(function(i){
-		var randomImg = arr.pop();{
-		$(this).attr("src",randomImg);
-		}
-	})
+// *****  function displayStats  ***** //
+function displayStats(){
+	var accuracy = match_counter/attempts.toFixed(0);
+	$("#games-played").text(games_played);
+	$("#attempts").text(attempts);
+	$("#accuracy").text((accuracy * 100).toFixed(0) +"%");
 }
-// function displayUpdate(){
-// }
+
+// ***** Reset Functions ***** //
+function resetFlips(){
+	// if(first_card_clicked !== null) {
+	// 	setTimeout(function(){
+	// 		if(second_card_clicked !== null){
+	// 			first_card_clicked.toggleClass("flipped");
+	// 			first_card_clicked = null;
+	// 			stopClick();
+	// 		}
+	// 	},1300)
+	// } else {
+		setTimeout(function(){
+			first_card_clicked.toggleClass("flipped");
+			second_card_clicked.toggleClass("flipped");
+			first_card_clicked = null;
+			second_card_clicked = null;
+		},1300)
+	// }
+}
+function resetGame(){
+	games_played++;
+	$(".game-area").html("");
+	generateCards();
+	$("#games-played").text("0");
+	$("#attempts").text("0");
+	$("#accuracy").text("0%");
+}
 
